297aded69270b31cf2d0d7f29736bb3eb9991e36
PedroBarge <pedro525tavares@gmail.com>
2023-11-22 12:22:16 +0000

    chance the index of delete

--- ListasArrayEx/ListaArrayEx/src/Main.java
+++ ListasArrayEx/ListaArrayEx/src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        List<String> list = new List<>("Root");
+        List<String> list = new List<>("");
         System.out.println("Hello world!");
 
-        list.add("tenho a 0 posição");
         list.add("tenho a 1 posição");
         list.add("tenho a 2 posição");
-        //list.addValue("World", 1);
+        list.add("tenho a 3 posição");
+        list.addValue("Agora eu", 1);
         list.getIndex(2);
-        list.deleteIndex(3);
+        list.deleteIndex(0);
     }
 }
